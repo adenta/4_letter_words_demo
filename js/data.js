faa330c7dd9776cd36452297d9fb4825643484b8
adenta <adenta@indiana.edu>
2015-09-13 22:00:50 -0500

    Fixed Pages

---
+++ js/data.js
@@ -0,0 +1,363 @@
+var data = [
+    {
+        "show_number": "3596",
+        "category": "E BEFORE I",
+        "air_date": "2000-04-03",
+        "answer": "Veil",
+        "value": "$200",
+        "round": "Jeopardy!",
+        "question": "'This item worn by a bride might be fingertip or cathedral length'"
+   },
+    {
+        "show_number": "3596",
+        "category": "NATIONAL ANTHEMS",
+        "air_date": "2000-04-03",
+        "answer": "Chad",
+        "value": "$500",
+        "round": "Jeopardy!",
+        "question": "'\"La Tchadienne\"'"
+   },
+    {
+        "show_number": "3854",
+        "category": "GADGETS",
+        "air_date": "2001-05-10",
+        "answer": "Tivo",
+        "value": "$400",
+        "round": "Jeopardy!",
+        "question": "'The Philips DSR6000 combines a DirecTV receiver with this brand's TV recording system'"
+   },
+    {
+        "show_number": "2124",
+        "category": "1954",
+        "air_date": "1993-11-25",
+        "answer": "Mile",
+        "value": "$100",
+        "round": "Jeopardy!",
+        "question": "'At an Oxford, England track meet on May 6, this distance was covered in a record 3:59.4 seconds'"
+   },
+    {
+        "show_number": "2124",
+        "category": "BOTTOMS UP!",
+        "air_date": "1993-11-25",
+        "answer": "Rose",
+        "value": "$100",
+        "round": "Jeopardy!",
+        "question": "'It's the generic term for pink or blush wines'"
+   },
+    {
+        "show_number": "3621",
+        "category": "ADJECTIVES IN NATURE",
+        "air_date": "2000-05-08",
+        "answer": "apes",
+        "value": "$100",
+        "round": "Jeopardy!",
+        "question": "'Chimps & gorillas are classified as \"great\" ones'"
+   },
+    {
+        "show_number": "3621",
+        "category": "THE GONE WITH THE WIND MOVIE MUSEUM",
+        "air_date": "2000-05-08",
+        "answer": "Tara",
+        "value": "$200",
+        "round": "Jeopardy!",
+        "question": "'One of the museum's most treasured movie artifacts is the front doorway of this plantation, Scarlett's home'"
+   },
+    {
+        "show_number": "5591",
+        "category": "AHH, LIVER!",
+        "air_date": "2008-12-22",
+        "answer": "clot",
+        "value": "$400",
+        "round": "Jeopardy!",
+        "question": "'Some substances made by the liver help fight disease; fibrinogen enables the blood to do this 4-letter function'"
+   },
+    {
+        "show_number": "5591",
+        "category": "PAINT YOUR WAGON",
+        "air_date": "2008-12-22",
+        "answer": "grey",
+        "value": "$600",
+        "round": "Jeopardy!",
+        "question": "'Battle wagon is another name for a battleship, so paint it the color called battleship this'"
+   },
+    {
+        "show_number": "5591",
+        "category": "SCIENCE & NATURE",
+        "air_date": "2008-12-22",
+        "answer": "mass",
+        "value": "$1600",
+        "round": "Double Jeopardy!",
+        "question": "'In science a big M can mean mach, & a little m this, as in Einstein's most famous equation'"
+   },
+    {
+        "show_number": "1429",
+        "category": "ESPANOL",
+        "air_date": "1990-11-15",
+        "answer": "jail",
+        "value": "$300",
+        "round": "Jeopardy!",
+        "question": "'The Spanish \"calabozo\" & the cowboy variation calaboose both mean thse'"
+   },
+    {
+        "show_number": "5342",
+        "category": "THE NEW MEXICO DESERT",
+        "air_date": "2007-11-27",
+        "answer": "lava",
+        "value": "$800",
+        "round": "Jeopardy!",
+        "question": "'The basin called Jornada del Muerto is often compared to a moonscape due to its barren flows of this'"
+   },
+    {
+        "show_number": "5758",
+        "category": "YOU CAN SAIL THE 7 SEAS",
+        "air_date": "2009-09-30",
+        "answer": "Sulu",
+        "value": "$400",
+        "round": "Double Jeopardy!",
+        "question": "'Oh, my! A George Takei TV character shares his last name with this sea between the Philippine Islands & Borneo'"
+   },
+    {
+        "show_number": "5435",
+        "category": "MOVIE ADJECTIVES",
+        "air_date": "2008-04-04",
+        "answer": "Wild",
+        "value": "$800",
+        "round": "Jeopardy!",
+        "question": "'1969: \"The ___ Bunch\"'"
+   },
+    {
+        "show_number": "3846",
+        "category": "PENN. NAMES",
+        "air_date": "2001-04-30",
+        "answer": "Erie",
+        "value": "$1,000",
+        "round": "Double Jeopardy!",
+        "question": "'Appropriately, this city is Pennsylvania's largest port on the Great Lakes'"
+   },
+    {
+        "show_number": "4628",
+        "category": "I KNOW THAT SONG",
+        "air_date": "2004-10-20",
+        "answer": "free",
+        "value": "$1200",
+        "round": "Double Jeopardy!",
+        "question": "'The song \"God Bless The U.S.A.\" says, \"I'm proud to be an American where at least I know I'm\" this'"
+   },
+    {
+        "show_number": "4511",
+        "category": "\"I\" REMEMBER",
+        "air_date": "2004-03-29",
+        "answer": "Isis",
+        "value": "$1600",
+        "round": "Double Jeopardy!",
+        "question": "'Io, a priestess at Argos in Greek myth, is identified with this Egyptian goddess'"
+   },
+    {
+        "show_number": "4084",
+        "category": "CHEMISTRY",
+        "air_date": "2002-05-09",
+        "answer": "spin",
+        "value": "$1600",
+        "round": "Double Jeopardy!",
+        "question": "'4-letter word for the intrinsic angular momentum of an elementary particle'"
+   },
+    {
+        "show_number": "5788",
+        "category": "A DAY WITH GRANDMA",
+        "air_date": "2009-11-11",
+        "answer": "yarn",
+        "value": "$400",
+        "round": "Double Jeopardy!",
+        "question": "'You stopped for some alpaca chunky, grandma's favorite type of this wool knitting essential'"
+   },
+    {
+        "show_number": "5788",
+        "category": "\"T\"EOGRAPHY",
+        "air_date": "2009-11-11",
+        "answer": "Togo",
+        "value": "$2000",
+        "round": "Double Jeopardy!",
+        "question": "'This small West African country is wedged between Ghana & Benin'"
+   },
+    {
+        "show_number": "5327",
+        "category": "AT THE BUFFET",
+        "air_date": "2007-11-06",
+        "answer": "gala",
+        "value": "$800",
+        "round": "Jeopardy!",
+        "question": "'How about some fruit salad featuring this apple variety whose name means \"a festive celebration\"'"
+   },
+    {
+        "show_number": "2886",
+        "category": "GEMS",
+        "air_date": "1997-03-03",
+        "answer": "Ruby",
+        "value": "$100",
+        "round": "Jeopardy!",
+        "question": "'Examples of this July birthstone found in Sri Lanka are typically a pale shade of red'"
+   },
+    {
+        "show_number": "2886",
+        "category": "SOUL & FUNK MUSIC",
+        "air_date": "1997-03-03",
+        "answer": "Kool",
+        "value": "$200",
+        "round": "Jeopardy!",
+        "question": "'Funk major Robert Bell is better known by this name, especially when working with \"The Gang\"'"
+   },
+    {
+        "show_number": "2886",
+        "category": "LESSER-KNOWN PROVERBS",
+        "air_date": "1997-03-03",
+        "answer": "Ears",
+        "value": "$400",
+        "round": "Double Jeopardy!",
+        "question": "'\"Fields have eyes and woods have\" these; \"walls have\" them, too'"
+   },
+    {
+        "show_number": "2126",
+        "category": "EGYPTOLOGY",
+        "air_date": "1993-11-29",
+        "answer": "Date",
+        "value": "$200",
+        "round": "Double Jeopardy!",
+        "question": "'An ancient Egyptian who had a sweet tooth would reach for honey or this palm fruit'"
+   },
+    {
+        "show_number": "6140",
+        "category": "FIRST NAMES IN THE DICTIONARY",
+        "air_date": "2011-04-29",
+        "answer": "nick",
+        "value": "$400",
+        "round": "Jeopardy!",
+        "question": "'To hit or injure slightly, or to give yourself a small cut while shaving--ouch!'"
+   },
+    {
+        "show_number": "4810",
+        "category": "GREEN \"T\"",
+        "air_date": "2005-07-01",
+        "answer": "turf",
+        "value": "$400",
+        "round": "Jeopardy!",
+        "question": "'Artificial green grass used on a playing field'"
+   },
+    
+    {
+        "show_number": "5568",
+        "category": "RHYME SQUAD",
+        "air_date": "2008-11-19",
+        "answer": "plod",
+        "value": "$800",
+        "round": "Jeopardy!",
+        "question": "'To walk heavily & wearily'"
+   },
+    {
+        "show_number": "6010",
+        "category": "U.S. MAGAZINES",
+        "air_date": "2010-10-29",
+        "answer": "AARP",
+        "value": "$200",
+        "round": "Jeopardy!",
+        "question": "'No. 1 in circulation: this retirees' organization's bulletin'"
+   },
+    {
+        "show_number": "3698",
+        "category": "POTPOURRI",
+        "air_date": "2000-10-04",
+        "answer": "cake",
+        "value": "$200",
+        "round": "Jeopardy!",
+        "question": "'Devil's food & angel food are types of this dessert'"
+   },
+    {
+        "show_number": "5657",
+        "category": "FLOORING",
+        "air_date": "2009-03-24",
+        "answer": "tile",
+        "value": "$200",
+        "round": "Jeopardy!",
+        "question": "'Grout & a special saw are tools for installing this type of floor'"
+   },
+    {
+        "show_number": "5657",
+        "category": "POETIC WORDS",
+        "air_date": "2009-03-24",
+        "answer": "ever",
+        "value": "$200",
+        "round": "Jeopardy!",
+        "question": "'E'er is a poetic form of this adverb'"
+   },
+    {
+        "show_number": "5657",
+        "category": "POETIC WORDS",
+        "air_date": "2009-03-24",
+        "answer": "even",
+        "value": "$400",
+        "round": "Jeopardy!",
+        "question": "'E'en is a poetic form of this adverb'"
+   },
+    {
+        "show_number": "5657",
+        "category": "POETIC WORDS",
+        "air_date": "2009-03-24",
+        "answer": "ebon",
+        "value": "$800",
+        "round": "Jeopardy!",
+        "question": "'\"L'allegro\" by Milton uses this 4-letter word for a deep shade of black, a shortening of the name of a wood'"
+   },
+    {
+        "show_number": "5964",
+        "category": "JONAH'S DRUTHERS",
+        "air_date": "2010-07-15",
+        "answer": "lots",
+        "value": "$200",
+        "round": "Jeopardy!",
+        "question": "'Aboard ship in a storm, the men \"cast\" these items of chance; Jonah's came up, but he'd rather it didn't'"
+   },
+    {
+        "show_number": "64",
+        "category": "CABLE TV",
+        "air_date": "1984-12-06",
+        "answer": "SCTV",
+        "value": "$900",
+        "round": "Jeopardy!",
+        "question": "'Comedy group which went from CBC to NBC to Cinemax'"
+   },
+    {
+        "show_number": "1333",
+        "category": "BROADWAY LYRICS",
+        "air_date": "1990-05-23",
+        "answer": "Head",
+        "value": "$300",
+        "round": "Jeopardy!",
+        "question": "'\"The sweetest sounds I'll ever hear are still inside\" this part of me'"
+   },
+    {
+        "show_number": "1333",
+        "category": "LITERATURE",
+        "air_date": "1990-05-23",
+        "answer": "Maud",
+        "value": "$800",
+        "round": "Double Jeopardy!",
+        "question": "'In an 1854 poem Tennyson asked her to \"Come Into The Garden\"'"
+   },
+    {
+        "show_number": "3388",
+        "category": "MATH TERMS",
+        "air_date": "1999-05-05",
+        "answer": "Base",
+        "value": "$200",
+        "round": "Jeopardy!",
+        "question": "'In our number system, it's 10; in a triangle, it's the lowest side'"
+   },
+    {
+        "show_number": "5748",
+        "category": "T\"OIL\"",
+        "air_date": "2009-09-16",
+        "answer": "soil",
+        "value": "$2000",
+        "round": "Double Jeopardy!",
+        "question": "'Nobel prize-winning Norwegian author Knut Hamsun wrote a tale about farming called \"growth of\" this'"
+   }
+];
